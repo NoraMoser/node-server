33981e5046983fc2b9ade85446189611fbc0f088
Moser <noralmoser@gmail.com>
2018-03-28 14:19:20 -0500

    using handlebars

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+// app.get('/', (request, response) => {
+//     // response.send('<h1>Hello, Express</h1>');
+//     response.send({
+//         name: 'Nora',
+//         likes: [
+//             'Reading', 'Relaxing', 'Eating'
+//         ]
+//     })
+// });
+
 app.get('/', (request, response) => {
-    // response.send('<h1>Hello, Express</h1>');
-    response.send({
-        name: 'Nora',
-        likes: [
-            'Reading', 'Relaxing', 'Eating'
-        ]
-    })
+    response.render('home.hbs', {
+        pageTitle: 'Nora\'s Home page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome to my website!'
+    });
 });
 
 app.get('/about', (request, response) => {
-    response.send('About Page');
-})
+    response.render('about.hbs', {
+        pageTitle: 'Nora\'s about page',
+        currentYear: new Date().getFullYear()
+    });
+});
 
 app.get('/bad', (request, response) => {
     response.send({
@@ -22,4 +37,6 @@ app.get('/bad', (request, response) => {
     })
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
