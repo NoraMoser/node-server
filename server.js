33b98091dbfe089d2bea619700c5b3e00e836d0c
Moser <noralmoser@gmail.com>
2018-03-28 15:05:18 -0500

    handlebars partials

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const hbs = require('hbs');
 
 var app = express();
 
+
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
 
 // app.get('/', (request, response) => {
 //     // response.send('<h1>Hello, Express</h1>');
@@ -19,15 +29,13 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (request, response) => {
     response.render('home.hbs', {
         pageTitle: 'Nora\'s Home page',
-        currentYear: new Date().getFullYear(),
         welcomeMessage: 'Welcome to my website!'
     });
 });
 
 app.get('/about', (request, response) => {
     response.render('about.hbs', {
-        pageTitle: 'Nora\'s about page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'Nora\'s about page'
     });
 });
 
